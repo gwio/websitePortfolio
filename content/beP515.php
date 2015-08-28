af3bb8086871a42d042a6879a4a8df6f65e70839
Christian Gwiozda <c.gwiozda@gmail.com>
2015-08-28 20:43:09 +0200

    vids in loop and beP5

--- content/beP515.php
+++ content/beP515.php
@@ -23,22 +23,54 @@ $cDir = $content . '/';
 
     </div>
 
-    <div class="row grey darken-2" id="masonry-grid">
+    <!----------------------->
+    <div class="row">
 
-      <!----------------------->
-      <?php include ($ROOT . "include/php/loadImg.php"); ?>
+    <div class="col s12 m12 l8 offset-l2">
+      <video class="responsive-video" controls type="video/mp4" width="auto" height="auto"  poster="<?=$cDir?>vids/bePoster_A.png">
+        <source src="<?=$cDir?>vids/beKidsP5_A.mp4" >
+        </video>
+      </div>
+
+      <div class="col s12 m12 l8 offset-l2">
+        <video class="responsive-video" controls type="video/mp4" width="auto" height="auto"  poster="<?=$cDir?>vids/bePoster_B.png">
+          <source src="<?=$cDir?>vids/beKidsP5_B.mp4" >
+          </video>
+        </div>
+
+
+        <div class="col s12 m12 l8 offset-l2">
+          <video class="responsive-video" controls type="video/mp4" width="auto" height="auto"  poster="<?=$cDir?>vids/bePoster_C.png">
+            <source src="<?=$cDir?>vids/beKidsP5_C.mp4" >
+            </video>
+          </div>
+
+
+          <div class="col s12 m12 l8 offset-l2">
+            <video class="responsive-video" controls type="video/mp4" width="auto" height="auto"  poster="<?=$cDir?>vids/bePoster_D.png">
+              <source src="<?=$cDir?>vids/beKidsP5_D.mp4" >
+              </video>
+            </div>
+
+      </div>
       <!----------------------->
 
+      <div class="row grey darken-2" id="masonry-grid">
+
+        <!----------------------->
+        <?php include ($ROOT . "include/php/loadImg.php"); ?>
+        <!----------------------->
+
+
+      </div>
 
     </div>
-
-  </div>
-</main>
+  </main>
 
 
-<!----------------------------------------------------------------------------->
+  <!----------------------------------------------------------------------------->
 
-<?php include ($ROOT . "include/php/fo.php"); ?>
+  <?php include ($ROOT . "include/php/fo.php"); ?>
 
-<!----------------------------------------------------------------------------->
-<!----------------------------------------------------------------------------->
+  <!----------------------------------------------------------------------------->
+  <!----------------------------------------------------------------------------->
--- content/loopMesh.php
+++ content/loopMesh.php
@@ -23,22 +23,60 @@ $cDir = $content . '/';
 
     </div>
 
-    <div class="row grey darken-2" id="masonry-grid">
+    <!------------------>
+    <div class="row">
 
-      <!----------------------->
-      <?php include ($ROOT . "include/php/loadImg.php"); ?>
-      <!----------------------->
-
-
-    </div>
-
-  </div>
-</main>
+      <div class="col s12 m12 l8 offset-l2">
+        <video class="responsive-video" controls type="video/mp4" width="auto" height="auto" loop poster="<?=$cDir?>vids/a-poster.png">
+          <source src="<?=$cDir?>vids/moebiusCapture_00.mp4" >
+          </video>
+        </div>
 
 
-<!----------------------------------------------------------------------------->
+        <div class="col s12 m12 l8 offset-l2">
+          <video class="responsive-video" controls type="video/mp4" width="auto" height="auto" loop poster="<?=$cDir?>vids/b-poster.png">
+            <source src="<?=$cDir?>vids/moebiusCapture_01.mp4" >
+            </video>
+          </div>
 
-<?php include ($ROOT . "include/php/fo.php"); ?>
 
-<!----------------------------------------------------------------------------->
-<!----------------------------------------------------------------------------->
+          <div class="col s12 m12 l8 offset-l2">
+            <video class="responsive-video" controls type="video/mp4" width="auto" height="auto" loop poster="<?=$cDir?>vids/c-poster.png">
+              <source src="<?=$cDir?>vids/moebiusCapture_02.mp4" >
+              </video>
+            </div>
+
+
+            <div class="col s12 m12 l8 offset-l2">
+              <video class="responsive-video" controls type="video/mp4" width="auto" height="auto" loop poster="<?=$cDir?>vids/d-poster.png">
+                <source src="<?=$cDir?>vids/moebiusCapture_03.mp4" >
+                </video>
+              </div>
+
+
+              <div class="col s12 m12 l8 offset-l2">
+                <video class="responsive-video" controls type="video/mp4" width="auto" height="auto" loop poster="<?=$cDir?>vids/e-poster.png">
+                  <source src="<?=$cDir?>vids/moebiusCapture_04.mp4" >
+                  </video>
+                </div>
+
+              </div>
+
+              <div class="row grey darken-2" id="masonry-grid">
+
+                <!----------------------->
+                <?php include ($ROOT . "include/php/loadImg.php"); ?>
+                <!----------------------->
+
+              </div>
+
+            </div>
+          </main>
+          
+
+          <!----------------------------------------------------------------------------->
+
+          <?php include ($ROOT . "include/php/fo.php"); ?>
+
+          <!----------------------------------------------------------------------------->
+          <!----------------------------------------------------------------------------->
