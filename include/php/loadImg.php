fe791dcc53387092ee16015cd478127b3a3e8842
Christian Gwiozda <c.gwiozda@gmail.com>
2015-09-05 00:29:45 +0200

    img alt text

--- include/php/loadImg.php
+++ include/php/loadImg.php
@@ -2,10 +2,12 @@
 $dir = $cDir . '*.png';
 $images = glob( $dir );
 
+$imgCount = 0;
 foreach( $images as $image ):
   if($image != $cDir . 'title.png'){
+    $imgCount += 1;
     echo '<div class="col s12 m12 l8">';
-    echo '<div class="section no-pad-top"><img src="' . $image . '" class="responsive-img z-depth-1"></div>';
+    echo '<div class="section no-pad-top"><img alt="' . $title . $imgCount . '" src="' . $image . '" class="responsive-img z-depth-1"></div>';
     echo '</div>';
   }
 endforeach;
