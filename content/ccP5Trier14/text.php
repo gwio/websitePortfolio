2bd389143f817c7c88b56bc599e4a6c24d801a6a
Christian Gwiozda <c.gwiozda@gmail.com>
2015-09-06 01:26:19 +0200

    txt

--- content/ccP5Trier14/text.php
+++ content/ccP5Trier14/text.php
@@ -2,7 +2,7 @@
 $indexName = "ccP5Trier14";
 $datum ="2014.07.07";
 $year = substr($datum,0,4);
-$title = "Creative Coding with Processing Course";
+$title = "Creative Coding with Processing";
 $tags = array("processing", "education", "lecture", "hochschule trier","code");
 $desc = 'Creative Coding with Processing was taught within the Faculty of Design at <a href="http://www.hochschule-trier.de/index.php?id=gestaltung" target="_blank" title="Hochschule Trier"> Hochschule Trier</a> during 2013-14. The aim of the lecture was to familiarize the students with basic programming principles and approaches to generative processes within the visual design. Most of the course material is also available on Github.';
 $links ='<a href="https://github.com/gwio/ccProcessingTrierWinter2013" target="_blank" title="Course Material on Github, 2013">Examples Winter</a><br>
--- content/modeLight/text.php
+++ content/modeLight/text.php
@@ -2,7 +2,7 @@
 $indexName = "modeLight";
 $datum ="2013.10.24";
 $year = substr($datum,0,4);
-$title = "Mapping for Fashion";
+$title = "Mapping on Fashion";
 $tags = array("openframeworks", "opencv", "projection", "realtime","code");
 $desc = 'Realtime projection mapping for a fashion design <a href="http://blog.modedesign-trier.de/?p=2727" title="Fashion Design Workshop" target="_blank">workshop</a> at Hochschule Trier. Images and designs are created by the participating students.';
 $links ='';
