0629e056dbfe53c7906cb3598f51d212c536a0b9
Christian Gwiozda <c.gwiozda@gmail.com>
2016-12-03 23:52:44 +0100

    added gridcomposer

---
+++ content/gridComposerApp.php
@@ -0,0 +1,59 @@
+<?php
+
+$ROOT = '../';
+
+$content = "gridComposerApp";
+$cDir = $content . '/';
+include_once($cDir . 'text.php');
+?>
+<!----------------------------------------------------------------------------->
+
+<?php include ($ROOT . "include/php/hd.php"); ?>
+
+<!----------------------------------------------------------------------------->
+<!----------------------------------------------------------------------------->
+
+<main>
+  <div class="container">
+
+
+    <!----------------------->
+
+    <?php include ($ROOT . "include/php/loadText.php"); ?>
+    <!----------------------->
+
+    <div class="row">
+      <div  class="col section no-pad-top" style="width:100%; max-width:1023px;">
+        <div class="video-container z-depth-1">
+          <iframe src="https://player.vimeo.com/video/192820464?byline=0"  frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div  class="col section no-pad-top" style="width:100%; max-width:1023px;">
+        <div class="video-container z-depth-1">
+          <iframe src="https://player.vimeo.com/video/192954003?byline=0"  frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+        </div>
+      </div>
+    </div>
+
+    <div class="row left-align" id="masonry-grid">
+
+      <!----------------------->
+      <?php include ($ROOT . "include/php/loadImg.php"); ?>
+      <!----------------------->
+
+
+    </div>
+
+  </div>
+</main>
+
+
+<!----------------------------------------------------------------------------->
+
+<?php include ($ROOT . "include/php/fo.php"); ?>
+
+<!----------------------------------------------------------------------------->
+<!----------------------------------------------------------------------------->
---
+++ content/gridComposerApp/text.php
@@ -0,0 +1,14 @@
+<?php
+$indexName = "gridComposerApp";
+$datum ="2016.11.15";
+$year = substr($datum,0,4);
+$title = "GridComposer App";
+$tags = array("sound", "code", "openframeworks", "sequencer", "midi", "iOS");
+$desc = 'GridComposer is an experimental musical sequencer for the iPhone and iPad.';
+$links ='<a href="http://gridcomposer.gwiozda.net" target="_blank" title="GridComposer Website">GridComposer Website</a><br>
+<a href="https://itunes.apple.com/us/app/gridcomposer/id993489006?ls=1&mt=8" target="_blank" title="Itunes Appstore Link">Download on iTunes</a><br>
+<a href="https://www.flickr.com/photos/22285762@N07/albums/72157676005230060" target="_blank" title="GridComposer App Screenshots">Image Gallery</a><br>
+<a href="https://www.flickr.com/photos/22285762@N07/albums/72157656705753375" target="_blank" title="GridComposer documentation and development images">Wip Image Gallery</a><br>
+<a href="https://github.com/gwio/gridComposerApp" target="_blank" title="Source Code on Github">Source on Github</a><br>
+';
+?>
